e929063214fc26bad59175f485f50eca4a6301b1
aritchat <aritrachatterjee2007@gmail.com>
2020-09-06 08:22:14 +0530

    Initial commit

---
+++ 03_PythonIntro/3.1.1_Numbers.py
@@ -0,0 +1,32 @@
+#Working with numbers
+print(2 + 2)
+
+print(50 -5*6)
+
+print((50 - 5*6) / 4)
+
+print(8 / 5)      # division always returns a floating point number
+
+print(17 / 3)     # classic division returns a float
+
+print(17 // 3)    # floor division discards the fractional part
+
+print(17 % 3)     # the % operator returns the remainder of the division
+
+print(5 * 3 + 2)  # result * divisor + remainder
+
+print(5 ** 2 )    # 5 squared
+
+print(2 ** 7)     # 2 to the power of 7
+
+width = 20
+height = 5 * 9
+print( width * height )
+
+#n                 # try to access an undefined variable
+
+print(4 * 3.75 - 1)
+
+tax = 12.5 / 100
+price = 100.50
+print( price * tax )
